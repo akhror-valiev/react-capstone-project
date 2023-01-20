9e69f0cf990e6225b86ed21d1de25c254052cc87
akhror-valiev <ahror.valiev@gmail.com>
2023-01-20 22:46:22 +0500

    Add Detail page and middleware thunk

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,26 +1,15 @@
 import React, { useEffect } from 'react';
-
 import { useDispatch } from 'react-redux';
+import { fetchAsyncMovies, fetchAsyncShows } from '../../redux/movies/movieSlice';
 import MovieList from '../MovieList.js/MovieList';
-import movieApi from '../../common/apis/MovieApi';
-import { APIKey } from '../../common/apis/MovieApiKey';
-import { addMovies } from '../../redux/movies/movieSlice';
 
 const Home = () => {
   const dispatch = useDispatch();
-  const movieText = 'Harry';
+
   useEffect(() => {
-    const fetchMovies = async () => {
-      const response = await movieApi
-        .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
-        .catch((err) => {
-          console.log('Err:', err);
-        });
-      dispatch(addMovies(response.data));
-      console.log('response from API', response);
-    };
-    fetchMovies();
-  }, []);
+    dispatch(fetchAsyncMovies());
+    dispatch(fetchAsyncShows());
+  }, [dispatch]);
 
   return (
     <div>
--- src/components/MovieList.js/MovieList.js
+++ src/components/MovieList.js/MovieList.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import uuid from 'react-uuid';
 import { useSelector } from 'react-redux';
-import { getAllMovies } from '../../redux/movies/movieSlice';
+import { getAllMovies, getAllShows } from '../../redux/movies/movieSlice';
 import MovieCard from '../MovieCard/MovieCard';
 import './MovieList.scss';
 
 const MovieList = () => {
   const movies = useSelector(getAllMovies);
+  const shows = useSelector(getAllShows);
+
   let renderMovies = '';
+  let renderShows = '';
 
   renderMovies = movies.Response === 'True' ? (
     movies.Search.map((movie) => (
@@ -18,12 +21,29 @@ const MovieList = () => {
       <h3>{movies.Error}</h3>
     </div>
   );
+
+  renderShows = shows.Response === 'True' ? (
+    shows.Search.map((show) => (
+      <MovieCard key={uuid()} data={show} />
+    ))
+  ) : (
+    <div className="show-error">
+      <h3>{shows.Error}</h3>
+    </div>
+  );
+
   return (
     <div className="movie-wrapper">
       <div className="movie-list">
         <h2>Movies</h2>
         <div className="movie-container">
           {renderMovies}
+        </div>
+      </div>
+      <div className="show-list">
+        <h2>Shows</h2>
+        <div className="movie-container">
+          {renderShows}
         </div>
       </div>
     </div>
