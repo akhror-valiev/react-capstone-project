e610e2408819243d65532f442bb3f8d5fccb3d45
akhror-valiev <ahror.valiev@gmail.com>
2023-01-22 00:08:28 +0500

    Update file

--- src/__tests__/Countries.test.js
+++ src/__tests__/Countries.test.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 import store from '../redux/configureStore';
-import Countries from '../components/continents/Continents';
+import Countries from '../components/Continents/Continents';
 
 describe('matches snapshot', () => {
   test('matches to snapshot', () => {
--- src/__tests__/Country.test.js
+++ src/__tests__/Country.test.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 import store from '../redux/configureStore';
-import Country from '../components/continents/Continents';
+import Country from '../components/Continents/Continents';
 
 describe('matches snapshot', () => {
   test('matches to snapshot', () => {
